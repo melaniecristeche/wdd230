0f42b8dc6a688a2b74125aee49a6805c465227b1
melaniecristeche <u943142818@byui.edu>
2022-03-11 18:07:23 -0300

    button and other stuff

--- chamber/js/responsive.js
+++ chamber/js/responsive.js
@@ -1,4 +1,4 @@
-// button responsive 
+// Menu responsive 
 
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
@@ -19,4 +19,18 @@ const fulldate = new Intl.DateTimeFormat('en-Us',{
 }).format(date);
 
 currentTime.innerHTML = fulldate;
+
+
+// Banner join us for the chamber meet and greet Wednesday at 7:00 p.m
+
+console.log(now.getDay())
+
+if (now.getDay() === 1 || now.getDay() == 2) {
+    datefield.innerHTML +=
+        `<p><a href='#'>
+    🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m
+    </a></p>`;
+    menu.classList.toggle('responsive-2');
+    document.querySelector('.pop').classList.toggle('space')
+}
 
