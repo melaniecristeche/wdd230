fab73454e54959dd41735008c15d1d6557ee9b51
melaniecristeche <u943142818@byui.edu>
2022-06-19 00:10:12 -0300

    ccc

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -15,16 +15,17 @@ function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
+    let img = document.createElement('img');
     let birthDate = document.createElement('p');
     let birthPlace = document.createElement('p');
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    let fullName = `${prophet.name} ${prophet.lastname}`
+    h2.innerHTML = `${fullName}`;
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}${ordinal_suffix_of(prophet.order)} Latter-day President`);
-    portrait.setAttribute('loading', 'lazy');
+    img.setAttribute('src', prophet.imageurl);
+    img.setAttribute('alt', `Portait of ${fullName} - ${prophet.order}${ordinal_suffix_of(prophet.order)} Latter-day President`);
+    img.setAttribute('loading', 'lazy');
     
     birthDate.innerHTML = `Date of Birth: <strong>${prophet.birthdate}</strong>`;
     birthPlace.innerHTML = `Place of Birth: <strong>${prophet.birthplace}</strong>`;
@@ -32,7 +33,7 @@ function displayProphets(prophet) {
     card.appendChild(h2);
     card.appendChild(birthDate);
     card.appendChild(birthPlace);
-    card.appendChild(portrait);
+    card.appendChild(img);
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
