08f52fcc19874461be13ec167b6598eaf68a9003
melaniecristeche <u943142818@byui.edu>
2022-06-20 23:46:51 -0300

    details finished

--- chamber/js/join.banner.js
+++ chamber/js/join.banner.js
@@ -3,7 +3,8 @@ const day = new Date();
 
 if (day.getDay() == 1 || day.getDay() == 2) { 
 
-	document.getElementById("datefield").innerHTML +=
-	`<p>
+	document.getElementById("join-banner").innerHTML +=
+
+	`<a href="join.html"><p>
 	🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m
-	</p>`;  }
+	</p></a>`;  }
