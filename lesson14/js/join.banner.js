21ec7f743840e81afcb948cc98950dc59cc173db
melaniecristeche <u943142818@byui.edu>
2022-07-21 02:03:52 -0300

    Final Project week 14 Home page

---
+++ lesson14/js/join.banner.js
@@ -0,0 +1,11 @@
+// Banner join us for the chamber meet and greet Wednesday at 7:00 p.m
+const day = new Date();
+
+if (day.getDay() == 1 || day.getDay() == 2) { 
+
+	document.getElementById("join-banner").innerHTML +=
+
+	`<a href="join.html"><p>
+	👋 Hi! Make a Hotel Reservation!
+	</p></a>`;  
+}
